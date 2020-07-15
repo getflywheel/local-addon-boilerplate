698e830e5deb356e5a9ecba943fc3625563756d7
Ethan Bütt <ethan.butt@getflywheel.com>
2020-07-15 17:00:56 -0500

    refactor: remove unused logic. Clean up structure.

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,12 @@ export default function (context) {
 	const { ipcMain } = electron;
 
 	ipcMain.on('save-count', async (event, siteId, count) => {
-		LocalMain.getServiceContainer().cradle.localLogger.log('info', `Received count ${count} for site ${siteId}.`);
+		LocalMain.getServiceContainer().cradle.localLogger.log('info', `Saving count ${count} for site ${siteId}.`);
 		LocalMain.SiteData.updateSite(siteId, {
 			id: siteId,
 			count,
 		});
-		// event.reply(replyChannel, getResponse(siteId));
 	});
-}
 
-function getResponse(siteId) {
-	const site = LocalMain.SiteData.getSite(siteId);
-	site;
-	return 0;
+
 }
