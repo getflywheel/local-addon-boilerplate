c2cd55e4ab332e2dd31761c5a08182f44f8a1d01
Ethan Bütt <ethan.butt@getflywheel.com>
2020-07-23 17:01:41 -0500

    feat: info popup now appears first time a site count is saved.

--- src/Boilerplate.jsx
+++ src/Boilerplate.jsx
@@ -1,7 +1,7 @@
 import * as LocalRenderer from '@getflywheel/local/renderer';
 import React, { Component } from 'react';
 import { ipcRenderer } from 'electron';
-import { Button } from '@getflywheel/local-components';
+import { Button, FlyModal, Title, Text } from '@getflywheel/local-components';
 
 export default class Boilerplate extends Component {
 	constructor(props) {
@@ -10,18 +10,37 @@ export default class Boilerplate extends Component {
 		this.state = {
 			siteId: props.match.params.siteID,
 			count: this.fetchCount(),
+			showInstructions: false,
 		};
 
-		// this.renderCount = this.renderCount.bind(this);
+		this.hideInstructions = this.hideInstructions.bind(this);
 		this.increaseCount = this.increaseCount.bind(this);
 		this.decreaseCount = this.decreaseCount.bind(this);
 		this.randomlySetCount = this.randomlySetCount.bind(this);
 		this.saveCount = this.saveCount.bind(this);
 	}
 
+	componentDidMount() {
+		ipcRenderer.once('instructions', (event) => {
+			this.setState({
+				showInstructions: true,
+			});
+		});
+	}
+
+	componentWillUnmount() {
+		ipcRenderer.removeAllListeners('instructions');
+	}
+
 	fetchCount() {
 		const site = this.props.sites[this.props.match.params.siteID],
 		return site.count ?? 0;
+	}
+
+	hideInstructions() {
+		this.setState({
+			showInstructions: false,
+		});
 	}
 
 	saveCount() {
@@ -57,10 +76,27 @@ export default class Boilerplate extends Component {
 		return <p>Count: {this.state.count}</p>;
 	}
 
+	renderInstructions() {
+		return (
+			<FlyModal
+				isOpen={this.state.showInstructions}
+				onRequestClose={this.hideInstructions}
+			>
+				<Title>Boilerplate Add-on</Title>
+				<Text>You just saved the count for this site! You can exit this add-on and return to find the count will remain the same (but only if you save!).</Text>
+				<Text>This is a boilerplate add-on to help you get started with development.</Text>
+				<Text>You will find a few examples in this add-on related to using Local Components, the Local add-on API, and other useful tools for making your add-on awesome!</Text>
+				<Text>Visit https://localwp.com/get-involved for more information about making an add-on for Local.</Text>
+				<Text>We can't wait to see what you create!</Text>
+			</FlyModal>
+		)
+	}
+
     render() {
         return (
             <div style={{ flex: '1', overflowY: 'auto', margin: '10px' }}>
                 <h2>Hello, World!</h2>
+				{this.renderInstructions()}
 				{this.renderCount()}
 				<div>
 					<Button onClick={this.increaseCount}>Increment Count</Button> &nbsp;
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import * as LocalMain from '@getflywheel/local/main';
 export default function (context) {
 	const { electron } = context;
 	const { ipcMain } = electron;
+	var didShowInstructions = false;
 
 	LocalMain.addIpcAsyncListener('get-random-count', async () => {
 		return Math.floor(Math.random() * 100);
 	});
 
 	ipcMain.on('save-count', async (event, siteId, count) => {
+		LocalMain.sendIPCEvent('instructions');
 		LocalMain.getServiceContainer().cradle.localLogger.log('info', `Saving count ${count} for site ${siteId}.`);
 		LocalMain.SiteData.updateSite(siteId, {
 			id: siteId,
