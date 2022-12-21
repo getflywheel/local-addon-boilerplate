d9b6e82eecc43fc96feaca01b0d4e01c13c92a4d
Benjamin Turner <ben.turner@wpengine.com>
2022-12-21 08:48:35 -0800

    feat: add example for creating a child log

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,23 @@ export default function (context: LocalMain.AddonMainContext): void {
 
 	const { localLogger, siteData } = LocalMain.getServiceContainer().cradle;
 
+	// Local uses Winston for logging which means we can create create child
+	// loggers with additional metadata. For example, the below child logger
+	// will log something like this within the Local log:
+	//
+	// {"thread":"main","addon":"boilerplate","level":"info","message":"Saving count 47 for site 5efOKun5u.","timestamp":"2022-12-21T16:43:40.515Z"}
+	const logger = localLogger.child({
+		thread: "main",
+		addon: "boilerplate",
+	});
+
 	LocalMain.addIpcAsyncListener("get-random-count", async () => {
 		return Math.floor(Math.random() * 100);
 	});
 
 	ipcMain.on("save-count", async (event, siteId, count) => {
 		LocalMain.sendIPCEvent("instructions");
-		localLogger.log("info", `Saving count ${count} for site ${siteId}.`);
+		logger.log("info", `Saving count ${count} for site ${siteId}.`);
 		siteData.updateSite(siteId, {
 			id: siteId,
 			boilerplateAddon: {
