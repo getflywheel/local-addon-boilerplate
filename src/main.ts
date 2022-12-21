7abb871ec5fe7eb6e8720d87542fb3329f993f93
Benjamin Turner <ben.turner@wpengine.com>
2022-12-21 11:21:11 -0800

    Modernize Boilerplate (#24)
    * chore: skip ts checking of libs
    
    * chore: upgrade @getflywheel/local and @getflywheel/local-components
    
    * chore: another upgrade to Local deps
    
    * fix: render Boilerplate component under the tools tab
    
    * fix: update save-count method to use new siteData class
    
    * feat: add example for creating a child log

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,36 @@
 // https://getflywheel.github.io/local-addon-api/modules/_local_main_.html
-import * as LocalMain from '@getflywheel/local/main';
+import * as Local from "@getflywheel/local";
+import * as LocalMain from "@getflywheel/local/main";
 
-export default function (context) {
+export default function (context: LocalMain.AddonMainContext): void {
+	// The context object allows us to intereact with various parts of Electron's main thread.
 	const { electron } = context;
 	const { ipcMain } = electron;
 
-	LocalMain.addIpcAsyncListener('get-random-count', async () => {
+	const { localLogger, siteData } = LocalMain.getServiceContainer().cradle;
+
+	// Local uses Winston for logging which means we can create create child
+	// loggers with additional metadata. For example, the below child logger
+	// will log something like this within the Local log:
+	//
+	// {"thread":"main","addon":"boilerplate","level":"info","message":"Saving count 47 for site 5efOKun5u.","timestamp":"2022-12-21T16:43:40.515Z"}
+	const logger = localLogger.child({
+		thread: "main",
+		addon: "boilerplate",
+	});
+
+	LocalMain.addIpcAsyncListener("get-random-count", async () => {
 		return Math.floor(Math.random() * 100);
 	});
 
-	ipcMain.on('save-count', async (event, siteId, count) => {
-		LocalMain.sendIPCEvent('instructions');
-		LocalMain.getServiceContainer().cradle.localLogger.log('info', `Saving count ${count} for site ${siteId}.`);
-		LocalMain.SiteData.updateSite(siteId, {
+	ipcMain.on("save-count", async (event, siteId, count) => {
+		LocalMain.sendIPCEvent("instructions");
+		logger.log("info", `Saving count ${count} for site ${siteId}.`);
+		siteData.updateSite(siteId, {
 			id: siteId,
-			count,
-		});
+			boilerplateAddon: {
+				count,
+			},
+		} as Partial<Local.SiteJSON>);
 	});
 }
--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,30 +1,28 @@
-import Boilerplate from './Boilerplate';
-import fs from 'fs-extra';
-import path from 'path';
+import Boilerplate from "./Boilerplate";
+import fs from "fs-extra";
+import path from "path";
 
-const packageJSON = fs.readJsonSync(path.join(__dirname, '../package.json'));
-const addonName = packageJSON['productName'];
-const addonID = packageJSON['slug'];
+const packageJSON = fs.readJsonSync(path.join(__dirname, "../package.json"));
+const addonID = packageJSON["slug"];
 
 export default function (context) {
 	const { React, hooks } = context;
-	const { Route } = context.ReactRouter;
 
-	// Create the route/page of content that will be displayed when the menu option is clicked
-	hooks.addContent('routesSiteInfo', () => <Route key={`${addonID}-addon`} path={`/main/site-info/:siteID/${addonID}`}
-		render={(props) => <Boilerplate {...props} />} />);
-
-
-	// Add menu option within the site menu bar
-	hooks.addFilter('siteInfoMoreMenu', function (menu, site) {
-		menu.push({
-			label: `${addonName}`,
-			enabled: true,
-			click: () => {
-				context.events.send('goToRoute', `/main/site-info/${site.id}/${addonID}`);
-			}
-		});
-
-		return menu;
+	/*
+	 * The `path` is relative to the context of this hook, which is the currently viewed site
+	 *
+	 * The full path would look something like `/main/site-info/:siteID/<below-path-var>`
+	 */
+	hooks.addFilter("siteInfoToolsItem", (menu) => {
+		return [
+			...menu,
+			{
+				menuItem: "Counter",
+				path: `/${addonID}`,
+				render: (props) => {
+					return <Boilerplate {...props} />;
+				},
+			},
+		];
 	});
 }
