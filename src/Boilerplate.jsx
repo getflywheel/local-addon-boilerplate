4ccad45e31fd3f975fdaa89d82597b8716579266
Benjamin Turner <ben.turner@wpengine.com>
2023-10-04 11:27:52 -0700

    chore: eslint --fix save
    Standardizing eslint rules from another addon meant a bunch of errors:
    
    ```
    ✖ 437 problems (9 errors, 428 warnings)
      0 errors and 345 warnings potentially fixable with the `--fix` option.
    ```
    
    This commit is the changes made by running:
    
    ```
    yarn eslint --fix src/main.ts src/Boilerplate.jsx src/renderer.jsx
    ```
    
    which leaves us with one remaining error:
    
    ```
    ✖ 1 problem (1 error, 0 warnings)
    ```

--- src/Boilerplate.jsx
+++ src/Boilerplate.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import { ipcRenderer } from "electron";
+import React, { Component } from 'react';
+import { ipcRenderer } from 'electron';
 
 // https://getflywheel.github.io/local-addon-api/modules/_local_renderer_.html
-import * as LocalRenderer from "@getflywheel/local/renderer";
+import * as LocalRenderer from '@getflywheel/local/renderer';
 
 // https://github.com/getflywheel/local-components
-import { Button, FlyModal, Title, Text } from "@getflywheel/local-components";
+import { Button, FlyModal, Title, Text } from '@getflywheel/local-components';
 
 export default class Boilerplate extends Component {
-	constructor(props) {
+	constructor (props) {
 		super(props);
 
 		this.state = {
@@ -24,69 +24,69 @@ export default class Boilerplate extends Component {
 		this.saveCount = this.saveCount.bind(this);
 	}
 
-	componentDidMount() {
-		ipcRenderer.once("instructions", (event) => {
+	componentDidMount () {
+		ipcRenderer.once('instructions', (event) => {
 			this.setState({
 				showInstructions: true,
 			});
 		});
 	}
 
-	componentWillUnmount() {
-		ipcRenderer.removeAllListeners("instructions");
+	componentWillUnmount () {
+		ipcRenderer.removeAllListeners('instructions');
 	}
 
-	fetchCount() {
+	fetchCount () {
 		return this.props.site.boilerplateAddon?.count ?? 0;
 	}
 
-	hideInstructions() {
+	hideInstructions () {
 		this.setState({
 			showInstructions: false,
 		});
 	}
 
-	saveCount() {
-		ipcRenderer.send("save-count", this.state.siteId, this.state.count);
+	saveCount () {
+		ipcRenderer.send('save-count', this.state.siteId, this.state.count);
 	}
 
-	increaseCount() {
+	increaseCount () {
 		const oldCount = this.state.count;
 		this.setState({
 			count: oldCount + 1,
 		});
 	}
 
-	decreaseCount() {
+	decreaseCount () {
 		const oldCount = this.state.count;
 		this.setState({
 			count: oldCount - 1,
 		});
 	}
 
-	async randomlySetCount() {
-		const newCount = await LocalRenderer.ipcAsync("get-random-count");
+	async randomlySetCount () {
+		const newCount = await LocalRenderer.ipcAsync('get-random-count');
 		this.setState({
 			count: newCount,
 		});
 	}
 
-	renderCount() {
+	renderCount () {
 		return <p>Count: {this.state.count}</p>;
 	}
 
-	renderInstructions() {
+	renderInstructions () {
 		return (
 			<FlyModal
 				isOpen={this.state.showInstructions}
 				onRequestClose={this.hideInstructions}
 			>
 				<Title fontSize="xl">Boilerplate Add-on</Title>
-				<div style={{ padding: "20px" }}>
+				<div style={{ padding: '20px' }}>
 					<Text
 						fontSize="l"
 						privateOptions={{
-							fontWeight: "medium",
+							fontWeight: 'medium',
 						}}
 					>
 						You just saved the count for this site! You can exit
@@ -95,10 +95,10 @@ export default class Boilerplate extends Component {
 						add-on to help you get started with development. You
 						will find a few examples in this add-on related to using
 						Local Components, the Local add-on API, and other useful
-						tools for making your add-on awesome! Visit{" "}
+						tools for making your add-on awesome! Visit{' '}
 						<a href="https://localwp.com/get-involved">
 							the Local webpage about add-ons
-						</a>{" "}
+						</a>{' '}
 						for more information about making an add-on for Local.
 						We can't wait to see what you create!
 					</Text>
@@ -107,24 +107,24 @@ export default class Boilerplate extends Component {
 		);
 	}
 
-	render() {
+	render () {
 		return (
-			<div style={{ flex: "1", overflowY: "auto", margin: "10px" }}>
+			<div style={{ flex: '1', overflowY: 'auto', margin: '10px' }}>
 				<h2>Hello, World!</h2>
 				{this.renderInstructions()}
 				{this.renderCount()}
 				<div>
 					<Button onClick={this.decreaseCount}>
 						Decrement Count
-					</Button>{" "}
+					</Button>{' '}
 					&nbsp;
 					<Button onClick={this.increaseCount}>
 						Increment Count
-					</Button>{" "}
+					</Button>{' '}
 					&nbsp;
 					<Button onClick={this.randomlySetCount}>
 						Randomize Count
-					</Button>{" "}
+					</Button>{' '}
 					&nbsp;
 					<Button onClick={this.saveCount}>Save Count</Button>
 				</div>
